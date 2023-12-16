69da1e66a290a1732b1808dd427502fe03f4a98a
Fady Mondy <EngFadyMondy@gmail.com>
2023-12-16 17:19:52 +0200

    fix lang dir

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,3 +10,19 @@
         </div>
     </div>
 </x-splade-data>
+<x-splade-script>
+    if(localStorage.getItem("splade") && typeof document !== undefined){
+    let spladeStorage = JSON.parse(localStorage.getItem("splade"));
+    let dark = spladeStorage?.admin?.dark;
+    document.body.classList[dark ? "add" : "remove"]("dark-scrollbars");
+    document.documentElement.classList[dark ? "add" : "remove"]("dark");
+    let htmlEl = document.querySelector("html");
+
+    if ("{{app()->getLocale()}}" === "ar") {
+    htmlEl.setAttribute("dir", "rtl");
+    } else {
+    htmlEl.setAttribute("dir", "ltr");
+    }
+    }
+
+</x-splade-script>
